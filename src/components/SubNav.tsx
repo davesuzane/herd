cdec3c1897fb75b9b89bd059ae1ee2784bac4aa4
david <davesuzane711@gmail.com>
2026-07-27 13:10:47 -0300

    bum

--- src/components/SubNav.tsx
+++ src/components/SubNav.tsx
@@ -26,7 +26,7 @@ export default function SubNav() {
         </Link>
         <Link
           href="/chat"
-          className="text-xs ml-auto font-mono text-ink-faint hover:text-ink-dim transition"
+          className="text-xs bg-black b-rounded ml-auto font-mono text-ink-faint hover:text-ink-dim transition"
         >
           Chat
         </Link>
